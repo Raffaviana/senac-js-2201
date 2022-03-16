e329ee17f1704a1cac12693c94a3bf1ad3193097
JACKSON RAFAEL DE JESUS VIANA <jackson.rjviana@senacsp.edu.br>
2022-03-16 20:23:55 -0300

    exeplo aula

--- dom.js
+++ dom.js
@@ -1,2 +1,20 @@
-console.log(window);
-window.alert('Olá Mundo')
+const p = document.getElementById('paragrafo');
+
+p.innerText = 'Meu texto!';
+
+const cores = [ 'blue',
+                'red',
+                'brown',
+                'yellow',
+                'pink'];
+
+for(i = 0, c = 0;  i < 10; i++, c++){
+
+    p.style.background = cores[c];
+
+    console.log(c + ':' + p.style.background);
+
+    if( c >= 6) c = -1;
+}
+
+console.log(p);
